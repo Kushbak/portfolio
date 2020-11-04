81208ae6f9d460755f3ec23e3c5e907872b9469e
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-11-05 01:29:45 +0600

    some refactoring, renaming and style changing

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,19 @@
-const gulp = require('gulp') // Подключаем Gulp
-const browserSync = require('browser-sync').create()
-const watch = require('gulp-watch')
-const sass = require('gulp-sass')
-const autoprefixer = require('gulp-autoprefixer')
-const sourcemaps = require('gulp-sourcemaps')
-const notify = require('gulp-notify')
-const plumber = require('gulp-plumber')
-const pug = require('gulp-pug')
-const del = require('del')
-
+const gulp = require('gulp'); // Подключаем Gulp
+const browserSync = require('browser-sync').create();
+const watch = require('gulp-watch');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const pug = require('gulp-pug');
+const del = require('del');
 
 // Таск для сборки Gulp файлов
-gulp.task('pug', (callback) => {
+gulp.task('pug', function (callback) {
     return gulp.src('./src/pug/pages/**/*.pug')
         .pipe(plumber({
-            errorHandler: notify.onError((err) => {
+            errorHandler: notify.onError(function (err) {
                 return {
                     title: 'Pug',
                     sound: false,
@@ -30,12 +29,11 @@ gulp.task('pug', (callback) => {
     callback();
 });
 
-
 // Таск для компиляции SCSS в CSS
-gulp.task('scss', (callback) => {
+gulp.task('scss', function (callback) {
     return gulp.src('./src/scss/main.scss')
         .pipe(plumber({
-            errorHandler: notify.onError((err) => {
+            errorHandler: notify.onError(function (err) {
                 return {
                     title: 'Styles',
                     sound: false,
@@ -52,7 +50,7 @@ gulp.task('scss', (callback) => {
         .pipe(gulp.dest('./build/css/'))
         .pipe(browserSync.stream())
     callback();
-}); 
+});
 
 gulp.task('copy:css', function (callback) {
     return gulp.src('./src/css/**/*.*')
@@ -61,43 +59,41 @@ gulp.task('copy:css', function (callback) {
 });
 
 // Копирование Изображений
-gulp.task('copy:img', (callback) => {
+gulp.task('copy:img', function (callback) {
     return gulp.src('./src/img/**/*.*')
         .pipe(gulp.dest('./build/img/'))
     callback();
 });
 
-
 // Копирование Скриптов
-gulp.task('copy:js', (callback) => {
+gulp.task('copy:js', function (callback) {
     return gulp.src('./src/js/**/*.*')
         .pipe(gulp.dest('./build/js/'))
     callback();
 });
 
-
 // Слежение за HTML и CSS и обновление браузера
-gulp.task('watch', () => {
+gulp.task('watch', function () {
 
     // Следим за картинками и скриптами и обновляем браузер
-    watch(['./build/js/**/*.*', './build/img/**/*.*'], gulp.parallel(browserSync.reload))
+    watch(['./build/js/**/*.*', './build/img/**/*.*'], gulp.parallel(browserSync.reload));
 
     // Запуск слежения и компиляции SCSS с задержкой
-    watch('./src/scss/**/*.scss', () => {
-        setTimeout(gulp.parallel('scss'), 1000)
+    watch('./src/scss/**/*.scss', function () {
+        setTimeout(gulp.parallel('scss', browserSync.reload), 500)
     })
 
     // Слежение за PUG и сборка
-    watch('./src/pug/**/*.pug', gulp.parallel('pug'))
+    watch('./src/pug/**/*.pug', gulp.parallel('pug', browserSync.reload))
 
     // Следим за картинками и скриптами, и копируем их в build
-    watch('./src/img/**/*.*', gulp.parallel('copy:img'))
-    watch('./src/js/**/*.*', gulp.parallel('copy:js'))  
+    watch('./src/img/**/*.*', gulp.parallel('copy:img', browserSync.reload))
+    watch('./src/js/**/*.*', gulp.parallel('copy:js', browserSync.reload))
+
 });
 
-
-// Задача для старта сервера из папки build
-gulp.task('server', () => {
+// Задача для старта сервера из папки app
+gulp.task('server', function () {
     browserSync.init({
         server: {
             baseDir: "./build/"
@@ -105,11 +101,9 @@ gulp.task('server', () => {
     })
 });
 
-
-gulp.task('clean:build', () => {
+gulp.task('clean:build', function () {
     return del('./build')
 });
-
 
 // Дефолтный таск (задача по умолчанию)
 // Запускаем одновременно задачи server и watch
@@ -120,4 +114,4 @@ gulp.task(
         gulp.parallel('scss', 'pug', 'copy:img', 'copy:js', 'copy:css'),
         gulp.parallel('server', 'watch'),
     )
-); 
+);
